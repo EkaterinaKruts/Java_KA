238f3b74ed9535878858ca6e29acb79fc88f85e4
EkaterinaKruts <katerunka90@gmail.com>
2021-12-02 18:39:47 -0500

    Kate's

---
+++ MyFirstApp.java
@@ -0,0 +1,21 @@
+
+public class MyFirstApp {
+  public static void main (String[] args) {
+  System.out.println("I Rule!");
+  System.out.println("The World!");
+  int x = 3;
+  String name = "Dirk";
+  x  = x * 17;
+  System.out.print ("x is " + x);
+  double d = Math.random();
+  //This is comment
+  while (x > 12){
+    x = x - 1;
+  }
+  for (int xi = 0; xi < 10; xi = xi + 1){
+    System.out.print ("x is now " + xi);
+  }
+
+
+  }
+}
