8bc6628cfa252d034cf1f136a04ba901cd7f3cc8
EkaterinaKruts <katerunka90@gmail.com>
2021-12-02 22:19:01 -0500

    Beer Song

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,4 +1,5 @@
 
+//Exersize 1
 public class MyFirstApp {
   public static void main (String[] args) {
   System.out.println("I Rule!");
@@ -15,7 +16,16 @@ public class MyFirstApp {
   for (int xi = 0; xi < 10; xi = xi + 1){
     System.out.print ("x is now " + xi);
   }
-
+// Jva has tree standart looping condtructs: while, do-while, and for.
+// Statements end in a semicolon;
+// Code blocks are defined by a pair of curly braces {}
+//Declare an integer variable with a name and type: int x;
+// The assignment operator is one equel sign =
+// The equel operator uses two equel sign ==
+// A while loop runs everything within its block (defined by curly braces) as long as the conditional test is true.
+// If the conditional test is false, the while loop code block won't run, and execution will move down to the code immediately after the loop block.
+// Put a boolean test inside parentheses: while (x == 4) {}
 
   }
 }
+//Ex 2
